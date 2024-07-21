d663e223a539d7d7a860d3a51577e5f83631fda2
Geri Zbobo <gerizbobo@Geris-MacBook-Pro-2019.local>
2024-07-21 23:37:41 +0200

    Add untracked files

---
+++ test-saranda/assets/js/swiper-init.js
@@ -0,0 +1,16 @@
+// swiper-init.js
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+  });
+  
